9af1cc77f792ab9e1b33fdf1a3768573e2cdd369
Choco Kim <devheedoo@gmail.com>
2021-12-07 03:32:44 +0900

    feat: Pass props on styled-components

--- src/App.js
+++ src/App.js
@@ -4,14 +4,8 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 200px;
-  height: 200px;
-`;
-
-const BoxTwo = styled.div`
-  background-color: tomato;
+const Box = styled.div`
+  background-color: ${(props) => props.backgroundColor};
   width: 200px;
   height: 200px;
 `;
@@ -19,8 +13,8 @@ const BoxTwo = styled.div`
 function App() {
   return (
     <Father>
-      <BoxOne />
-      <BoxTwo />
+      <Box backgroundColor="tomato" />
+      <Box backgroundColor="teal" />
     </Father>
   );
 }
