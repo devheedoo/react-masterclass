7d737a9475b4f461c55caf966ab405d5921c3a2d
Choco Kim <devheedoo@gmail.com>
2021-12-07 03:28:17 +0900

    feat: Convert boxes with styled-components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import styled from 'styled-components';
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 200px;
+  height: 200px;
+`;
+
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
+
 function App() {
   return (
-    <div style={{ display: 'flex' }}>
-      <div style={{ backgroundColor: 'teal', width: 200, height: 200 }}></div>
-      <div style={{ backgroundColor: 'tomato', width: 200, height: 200 }}></div>
-    </div>
+    <Father>
+      <BoxOne />
+      <BoxTwo />
+    </Father>
   );
 }
 
