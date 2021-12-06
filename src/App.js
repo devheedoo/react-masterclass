beb8c2bfffbb587af4f45875cbc259b43acd0723
Choco Kim <devheedoo@gmail.com>
2021-12-07 04:28:19 +0900

    feat: ThemeProvider for theme change

--- src/App.js
+++ src/App.js
@@ -6,48 +6,17 @@ const Wrapper = styled.div`
   height: 100vh;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
-const toCircle = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 48px;
-`;
-
-const Box = styled.div`
-  width: 200px;
-  height: 200px;
-  background-color: teal;
-  animation: ${toCircle} 1s linear infinite;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  ${Emoji} {
-    &:hover {
-      font-size: 96px;
-    }
-  }
+const Title = styled.h2`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>🤩</Emoji>
-      </Box>
-      <Emoji>🔥</Emoji>
+      <Title>Hello!</Title>
     </Wrapper>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ThemeProvider } from 'styled-components';
+
+const lightTheme = {
+  textColor: 'black',
+  backgroundColor: 'lightgreen',
+};
+
+const darkTheme = {
+  textColor: 'lightgreen',
+  backgroundColor: 'black',
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
