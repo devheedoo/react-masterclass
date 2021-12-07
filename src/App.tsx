71517eb58b794acf8496df49b26811202b556e39
Heedo Kim (Choco) on MacBook <devheedoo@gmail.com>
2021-12-07 14:05:37 +0900

    chore: Cleanup codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,5 @@
-import styled, { keyframes } from 'styled-components';
-
-const Wrapper = styled.div`
-  display: flex;
-  width: 100vw;
-  height: 100vh;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
-
-const Title = styled.h2`
-  color: ${(props) => props.theme.textColor};
-`;
-
 function App() {
-  return (
-    <Wrapper>
-      <Title>Hello!</Title>
-    </Wrapper>
-  );
+  return null;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { ThemeProvider } from 'styled-components';
-
-const lightTheme = {
-  textColor: 'black',
-  backgroundColor: 'lightgreen',
-};
-
-const darkTheme = {
-  textColor: 'lightgreen',
-  backgroundColor: 'black',
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
