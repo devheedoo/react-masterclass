7d43b9755883acd0c59d707bdc9bac23d2a90522
Heedo Kim (Choco) on MacBook <devheedoo@gmail.com>
2021-12-08 06:48:23 +0900

    feat(3.2): Type props

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Circle from "./Circle";
+
 function App() {
-  return null;
+  return (
+    <div>
+      <Circle backgroundColor="teal" />
+      <Circle backgroundColor="tomato" />
+    </div>
+  );
 }
 
 export default App;
