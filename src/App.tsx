c7436acd8b8ac1d212273a198a0e74c99e718242
Heedo Kim (Choco) on MacBook <devheedoo@gmail.com>
2021-12-10 03:41:26 +0900

    chore: Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,5 @@
-import React, { useState } from "react";
-
 function App() {
-  const [value, setValue] = useState("");
-
-  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setValue(event.currentTarget.value);
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(value);
-  };
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Type text"
-          value={value}
-          onChange={handleChange}
-        />
-        <button>Print console log</button>
-      </form>
-    </div>
-  );
+  return null;
 }
 
 export default App;
