3d4bd97308cf9f9bbe902a90e48b252e1b22915b
Heedo Kim (Choco) on MacBook <devheedoo@gmail.com>
2021-12-09 10:13:42 +0900

    feat(3.5): Add onChange event with TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import React, { useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [value, setValue] = useState("");
+
+  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setValue(event.currentTarget.value);
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          placeholder="Type text"
+          value={value}
+          onChange={handleChange}
+        />
+        <button>Print console log</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
