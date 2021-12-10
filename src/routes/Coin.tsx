565d64256e08cf6162c45590592b55c353c4a3c8
Heedo Kim (Choco) on MacBook <devheedoo@gmail.com>
2021-12-11 02:05:21 +0900

    feat(4.6): Add types to coin data

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -32,24 +32,68 @@ interface RouteState {
   name: string;
 }
 
+interface ICoin {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  // tags: object;
+  // team: object;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  // links: object;
+  // links_extended: object;
+  // whitepaper: object;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface ITicker {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      price: number;
+    };
+  };
+}
+
 function Coin() {
   const [isLoading, setLoading] = useState(true);
-  const [detail, setDetail] = useState({});
-  const [price, setPrice] = useState({});
+  const [coin, setCoin] = useState<ICoin>();
+  const [ticker, setTicker] = useState<ITicker>();
 
   const { coinId } = useParams<RouteParams>();
   const { state } = useLocation<RouteState>();
 
   useEffect(() => {
     (async () => {
-      const detailJSON = await (
+      const coinJSON = await (
         await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
       ).json();
-      const priceJSON = await (
+      const tickerJSON = await (
         await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
       ).json();
-      setDetail(detailJSON); // description
-      setPrice(priceJSON); // quotes.USD.price
+      setCoin(coinJSON); // description
+      setTicker(tickerJSON); // quotes.USD.ticker
       setLoading(false);
     })();
   }, []);
