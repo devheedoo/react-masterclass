3242b086d0d5732e34762819ad64ffe5e913d6c1
Heedo Kim (Choco) on MacBook <devheedoo@gmail.com>
2021-12-11 05:12:00 +0900

    feat(4.14): Draw charts with Apexcharts

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,15 +1,63 @@
 import { useQuery } from "react-query";
 import { fetchOhlcv } from "../api";
+import ApexChart from "react-apexcharts";
 
 interface ChartProps {
   coinId: string;
 }
 
+interface IOhlcv {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data: ohlcv } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data: ohlcv } = useQuery<IOhlcv[]>(["ohlcv", coinId], () =>
     fetchOhlcv(coinId)
   );
-  return <h1>Chart</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: ohlcv?.map((price) => price.close),
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              labels: { show: false },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
