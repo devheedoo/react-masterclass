aec479fc284ee42b80390cc27f8105fb1a673815
Heedo Kim (Choco) on MacBook <devheedoo@gmail.com>
2021-12-14 05:19:04 +0900

    feat(5.6): Show input length error

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -2,13 +2,18 @@ import React, { useState } from "react";
 
 function TodoList() {
   const [todo, setTodo] = useState("");
+  const [todoError, setTodoError] = useState("");
 
   const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setTodoError("");
     setTodo(event.currentTarget.value);
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    if (todo.length <= 10) {
+      return setTodoError("todo should be longer than 10");
+    }
     console.log(todo);
   };
 
@@ -17,6 +22,7 @@ function TodoList() {
       <form onSubmit={handleSubmit}>
         <input onChange={handleChange} placeholder="Add a todo" />
         <button>add</button>
+        {todoError !== "" ? todoError : null}
       </form>
     </div>
   );
