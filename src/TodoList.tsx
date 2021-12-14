84e5e777be98e56026b7c483df0243cb54b733a3
Choco Kim <devheedoo@gmail.com>
2021-12-15 04:04:29 +0900

    feat(5.8): Add pattern and customize inputs

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -14,19 +14,77 @@ function TodoList() {
         onSubmit={handleSubmit(onValid)}
       >
         <input
-          {...register('name', {
-            required: 'name is required',
+          {...register('userId', {
+            required: 'ID is required',
             minLength: {
               value: 5,
-              message: 'length of name should be longer than 4',
+              message: 'Length of first name should be longer than 4',
+            },
+            pattern: {
+              value: /^[A-Za-z0-9]$/,
+              message: 'Only alphabet and number are allowed for ID',
             },
           })}
-          placeholder="Add a name"
+          placeholder="ID"
         />
-        <input {...register('age')} placeholder="Add a age" />
-        <input {...register('firstName')} placeholder="Add a first name" />
-        <input {...register('lastName')} placeholder="Add a last name" />
-        <input {...register('todo')} placeholder="Add a todo" />
+        <input
+          {...register('password', {
+            required: 'Password is required',
+            minLength: {
+              value: 8,
+              message: 'Length of first name should be longer than 7',
+            },
+            pattern: {
+              value: /^[A-Za-z0-9]$/,
+              message: 'Only alphabet and number are allowed for password',
+            },
+          })}
+          placeholder="password"
+        />
+        <input
+          {...register('firstName', {
+            required: 'first name is required',
+            pattern: {
+              value: /^[A-Za-z0-9]$/,
+              message: 'Only alphabet and number are allowed for first name',
+            },
+          })}
+          placeholder="first name"
+        />
+        <input
+          {...register('lastName', {
+            required: 'last name is required',
+            pattern: {
+              value: /^[A-Za-z0-9]$/,
+              message: 'Only alphabet and number are allowed for last name',
+            },
+          })}
+          placeholder="last name"
+        />
+        <input
+          {...register('email', {
+            required: 'Email is required',
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@gmail.com$/,
+              message: 'only @gmail.com emails are allowed',
+            },
+          })}
+          placeholder="email"
+        />
+        <input
+          {...register('username', {
+            required: 'Username is required',
+            minLength: {
+              value: 5,
+              message: 'Length of name should be longer than 4',
+            },
+            pattern: {
+              value: /^[A-Za-z0-9]$/,
+              message: 'Only alphabet and number are allowed for username',
+            },
+          })}
+          placeholder="username"
+        />
         <button>add</button>
       </form>
     </div>
