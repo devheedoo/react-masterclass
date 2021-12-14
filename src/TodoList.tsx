5e84d04cce7579041c51ced403b61cda80aaa7fd
Choco Kim <devheedoo@gmail.com>
2021-12-15 03:24:49 +0900

    style: Add flex with column direction

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,16 +1,16 @@
-import { useForm } from "react-hook-form";
+import { useForm } from 'react-hook-form';
 
 function TodoList() {
   const { register } = useForm();
 
   return (
     <div>
-      <form>
-        <input {...register("name")} placeholder="Add a name" />
-        <input {...register("age")} placeholder="Add a age" />
-        <input {...register("firstName")} placeholder="Add a first name" />
-        <input {...register("lastName")} placeholder="Add a last name" />
-        <input {...register("todo")} placeholder="Add a todo" />
+      <form style={{ display: 'flex', flexDirection: 'column' }}>
+        <input {...register('name')} placeholder="Add a name" />
+        <input {...register('age')} placeholder="Add a age" />
+        <input {...register('firstName')} placeholder="Add a first name" />
+        <input {...register('lastName')} placeholder="Add a last name" />
+        <input {...register('todo')} placeholder="Add a todo" />
         <button>add</button>
       </form>
     </div>
