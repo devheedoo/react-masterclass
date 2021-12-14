13b64b6a2457cc0eae895dea732830aa0772030c
Choco Kim <devheedoo@gmail.com>
2021-12-15 08:14:50 +0900

    feat: Improve status validation

--- src/components/CreateStatus.tsx
+++ src/components/CreateStatus.tsx
@@ -8,20 +8,26 @@ interface IForm {
 
 function CreateStatus() {
   const [statuses, setStatuses] = useRecoilState(statusesAtom);
-  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const { register, handleSubmit, setValue, formState } = useForm<IForm>();
   const handleValid = ({ status }: IForm) => {
     console.log('Add:', status);
-    if (statuses.includes(status)) {
-      console.log(`${status} already exists`);
-      return;
-    }
     setStatuses([...statuses, status]);
     setValue('status', '');
   };
   return (
     <form onSubmit={handleSubmit(handleValid)}>
-      <input {...register('status')} type="text" placeholder="Add a status" />
+      <input
+        {...register('status', {
+          validate: (status) =>
+            statuses.includes(status)
+              ? `ERROR: ${status} already exists.`
+              : true,
+        })}
+        type="text"
+        placeholder="Add a status"
+      />
       <button>Add</button>
+      <span>{formState.errors?.status?.message}</span>
     </form>
   );
 }
