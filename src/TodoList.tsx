1b8861ad7698e2fe3268635b95ef201a20417f6e
Choco Kim <devheedoo@gmail.com>
2021-12-15 03:41:01 +0900

    feat(5.7): validation with react-hook-form
    - second parameter of register
    - formState

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,10 +1,11 @@
 import { useForm } from 'react-hook-form';
 
 function TodoList() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, formState } = useForm();
   const onValid = (data: any) => {
     console.log(data);
   };
+  console.log(formState.errors);
 
   return (
     <div>
@@ -12,7 +13,16 @@ function TodoList() {
         style={{ display: 'flex', flexDirection: 'column' }}
         onSubmit={handleSubmit(onValid)}
       >
-        <input {...register('name')} placeholder="Add a name" />
+        <input
+          {...register('name', {
+            required: 'name is required',
+            minLength: {
+              value: 5,
+              message: 'length of name should be longer than 4',
+            },
+          })}
+          placeholder="Add a name"
+        />
         <input {...register('age')} placeholder="Add a age" />
         <input {...register('firstName')} placeholder="Add a first name" />
         <input {...register('lastName')} placeholder="Add a last name" />
