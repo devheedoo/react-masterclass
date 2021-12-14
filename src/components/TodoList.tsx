ab0436b95ac690a3104783b85603a9adf78f1c9d
Choco Kim <devheedoo@gmail.com>
2021-12-15 05:46:08 +0900

    feat(5.11): Add todo with recoil state

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,19 +1,39 @@
 import { useForm } from 'react-hook-form';
+import { atom, useRecoilState } from 'recoil';
 
 interface IForm {
   todo: string;
 }
 
+interface ITodo {
+  id: number; // using Date
+  text: string;
+  status: 'TODO' | 'DOING' | 'IN_REVIEW' | 'DONE';
+}
+
+const todosAtom = atom<ITodo[]>({
+  key: 'todos',
+  default: [],
+});
+
 function TodoList() {
+  const [todos, setTodos] = useRecoilState(todosAtom);
   const { register, handleSubmit, setValue } = useForm<IForm>();
 
-  const handleValid = (values: IForm) => {
-    console.log('Add:', values.todo);
+  const handleValid = ({ todo }: IForm) => {
+    console.log('Add:', todo);
+    const newTodo: ITodo = {
+      id: new Date().getTime(),
+      text: todo,
+      status: 'TODO',
+    };
+    setTodos((currentTodos) => [newTodo, ...currentTodos]);
     setValue('todo', '');
   };
 
   return (
     <div>
+      <h1>TODOLIST</h1>
       <form onSubmit={handleSubmit(handleValid)}>
         <input {...register('todo')} type="text" placeholder="Add a todo" />
         <button>Add</button>
