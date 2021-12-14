d42270b3ad6f3786971710a722d0636f3a015624
Choco Kim <devheedoo@gmail.com>
2021-12-15 06:10:24 +0900

    refactor(5.12): Divide CreateTodo

---
+++ src/components/CreateTodo.tsx
@@ -0,0 +1,30 @@
+import { useForm } from 'react-hook-form';
+import { useSetRecoilState } from 'recoil';
+import { ITodo, todosAtom, TodoStatus } from '../atoms/todosAtom';
+
+interface IForm {
+  todo: string;
+}
+
+function CreateTodo() {
+  const setTodos = useSetRecoilState(todosAtom);
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const handleValid = ({ todo }: IForm) => {
+    console.log('Add:', todo);
+    const newTodo: ITodo = {
+      id: new Date().getTime(),
+      text: todo,
+      status: TodoStatus.TODO,
+    };
+    setTodos((currentTodos) => [newTodo, ...currentTodos]);
+    setValue('todo', '');
+  };
+  return (
+    <form onSubmit={handleSubmit(handleValid)}>
+      <input {...register('todo')} type="text" placeholder="Add a todo" />
+      <button>Add</button>
+    </form>
+  );
+}
+
+export default CreateTodo;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,34 +1,15 @@
-import { useForm } from 'react-hook-form';
-import { useRecoilState } from 'recoil';
-import { ITodo, todosAtom, TodoStatus } from '../atoms/todosAtom';
+import { useRecoilValue } from 'recoil';
+import { todosAtom } from '../atoms/todosAtom';
+import CreateTodo from './CreateTodo';
 import Todo from './Todo';
 
-interface IForm {
-  todo: string;
-}
-
 function TodoList() {
-  const [todos, setTodos] = useRecoilState(todosAtom);
-  const { register, handleSubmit, setValue } = useForm<IForm>();
-
-  const handleValid = ({ todo }: IForm) => {
-    console.log('Add:', todo);
-    const newTodo: ITodo = {
-      id: new Date().getTime(),
-      text: todo,
-      status: TodoStatus.TODO,
-    };
-    setTodos((currentTodos) => [newTodo, ...currentTodos]);
-    setValue('todo', '');
-  };
+  const todos = useRecoilValue(todosAtom);
 
   return (
     <div>
       <h1>TODOLIST</h1>
-      <form onSubmit={handleSubmit(handleValid)}>
-        <input {...register('todo')} type="text" placeholder="Add a todo" />
-        <button>Add</button>
-      </form>
+      <CreateTodo />
       <ul>
         {todos.map((todo) => (
           <Todo {...todo} />
