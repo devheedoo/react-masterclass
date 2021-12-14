9e50caca88fa2cfd5b3d49e3aeb30ffece32e488
Choco Kim <devheedoo@gmail.com>
2021-12-15 08:45:58 +0900

    refactor: Use constants for default statuses

--- src/states/statusAtom.ts
+++ src/states/statusAtom.ts
@@ -1,11 +1,13 @@
 import { atom } from 'recoil';
 
+export const DEFAULT_STATUSES = ['TODO', 'DOING', 'DONE'];
+
 export const statusAtom = atom<string>({
   key: 'status',
-  default: 'todo',
+  default: DEFAULT_STATUSES[0],
 });
 
 export const statusesAtom = atom<string[]>({
   key: 'statuses',
-  default: ['todo', 'doing', 'done'],
+  default: DEFAULT_STATUSES,
 });
