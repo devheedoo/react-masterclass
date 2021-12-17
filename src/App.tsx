49040bd4de60a6714d44c7c799001a811b249cdf
Choco Kim <devheedoo@gmail.com>
2021-12-18 02:02:02 +0900

    feat(7.11): AnimatePresence, exit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { motion, useMotionValue, Variants } from 'framer-motion';
+import { AnimatePresence, motion, Variants } from 'framer-motion';
+import { useState } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled(motion.div)`
@@ -8,46 +9,53 @@ const Wrapper = styled(motion.div)`
   justify-content: center;
   align-items: center;
   background: linear-gradient(135deg, #81ecec, #c23699);
+  flex-direction: column;
 `;
 
-const AirbnbSvg = styled.svg`
-  width: 200px;
-  height: 200px;
-  stroke: white;
-  stroke-width: 5px;
+const WhiteBox = styled(motion.div)`
+  position: absolute;
+  top: 250px;
+  width: 300px;
+  height: 100px;
+  background-color: rgba(255, 255, 255, 0.7);
+  margin: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 20px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
 `;
 
-const pathVariants: Variants = {
-  start: {
-    pathLength: 0,
-    fill: 'rgba(255,255,255,0)',
-  },
-  end: {
-    pathLength: 1,
-    fill: 'rgba(255,255,255,1)',
-  },
+const boxVariants: Variants = {
+  start: { opacity: 0, y: 10 },
+  visible: { opacity: 1, y: 0 },
+  invisible: { opacity: 0, y: 10 },
 };
 
 function App() {
+  const [isShowing, setShowing] = useState(false);
+  const toggleShowing = () => {
+    setShowing((curr) => !curr);
+  };
   return (
     <Wrapper>
-      <AirbnbSvg
-        focusable="false"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 448 512"
-      >
-        <motion.path
-          variants={pathVariants}
-          initial="start"
-          animate="end"
-          transition={{
-            default: { duration: 5 },
-            fill: { delay: 2, duration: 1 },
-          }}
-          fill="transparent"
-          d="M224 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z"
-        ></motion.path>
-      </AirbnbSvg>
+      <AnimatePresence>
+        {isShowing ? (
+          <WhiteBox
+            variants={boxVariants}
+            initial="start"
+            animate="visible"
+            exit="invisible"
+            transition={{
+              default: { duration: 1 },
+              opacity: { duration: 0.5 },
+            }}
+          >
+            Hello!
+          </WhiteBox>
+        ) : null}
+      </AnimatePresence>
+      <button onClick={toggleShowing}>Toggle</button>
     </Wrapper>
   );
 }
