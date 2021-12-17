5fd9a59e20a8160618e07d3d536f1f41875100a7
Choco Kim <devheedoo@gmail.com>
2021-12-18 00:11:22 +0900

    chore(7.5): Cleanup codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { motion, Variants } from 'framer-motion';
+import { motion } from 'framer-motion';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -14,49 +14,13 @@ const Box = styled(motion.div)`
   height: 200px;
   background-color: rgba(255, 255, 255, 0.2);
   border-radius: 50px;
-  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
 `;
-
-const Circle = styled(motion.div)`
-  width: 70px;
-  height: 70px;
-  place-self: center;
-  border-radius: 35px;
-  background-color: white;
-  opacity: 0.5;
-`;
-
-const boxAnimation: Variants = {
-  start: { scale: 0.0, opacity: 0 },
-  end: {
-    scale: 1,
-    opacity: 1,
-    transition: {
-      type: 'spring',
-      bounce: 0.5,
-      duration: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.1,
-    },
-  },
-};
-
-const circleAnimation = {
-  start: { opacity: 0, y: 10 },
-  end: { opacity: 0.5, y: 0 },
-};
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxAnimation} initial="start" animate="end">
-        <Circle variants={circleAnimation} />
-        <Circle variants={circleAnimation} />
-        <Circle variants={circleAnimation} />
-        <Circle variants={circleAnimation} />
-      </Box>
+      <Box />
     </Wrapper>
   );
 }
