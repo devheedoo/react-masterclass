5d25206616ca37038243961658c8d55a22455e6f
Choco Kim <devheedoo@gmail.com>
2021-12-18 02:50:28 +0900

    feat(7.15): Animate only with layout

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,9 @@ const Wrapper = styled(motion.div)`
   background: linear-gradient(135deg, #81ecec, #c23699);
 `;
 
-const WhiteBox = styled(motion.div)`
-  width: 100px;
-  height: 100px;
+const WhiteBox = styled.div`
+  width: 200px;
+  height: 200px;
   background-color: rgba(255, 255, 255, 0.7);
   margin: 10px;
   display: flex;
@@ -23,10 +23,29 @@ const WhiteBox = styled(motion.div)`
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
 `;
 
+const Content = styled(motion.div)`
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  background-color: rgba(137, 40, 40, 0.7);
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+`;
+
 function App() {
+  const [isClicked, setClicked] = useState(false);
+  const toggleClick = () => setClicked((prev) => !prev);
   return (
-    <Wrapper>
-      <WhiteBox></WhiteBox>
+    <Wrapper onClick={toggleClick}>
+      <WhiteBox>
+        {!isClicked ? (
+          <Content layoutId="content" style={{ borderRadius: 25 }} />
+        ) : null}
+      </WhiteBox>
+      <WhiteBox>
+        {isClicked ? (
+          <Content layoutId="content" style={{ borderRadius: 0, scale: 2 }} />
+        ) : null}
+      </WhiteBox>
     </Wrapper>
   );
 }
