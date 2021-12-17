943b95f8e8345844f419d247abf70e380e23ba55
Choco Kim <devheedoo@gmail.com>
2021-12-18 00:26:53 +0900

    feat(7.6): 'Drag'

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import { motion, Variants } from 'framer-motion';
+import { useRef } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
   width: 100vw;
   height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BigBox = styled.div`
+  width: 400px;
+  height: 400px;
+  background-color: rgba(255, 255, 255, 0.1);
+  border-radius: 50px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
   display: flex;
   justify-content: center;
   align-items: center;
@@ -23,9 +35,19 @@ const boxVariants: Variants = {
 };
 
 function App() {
+  const bigBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box variants={boxVariants} whileHover="hover" whileTap="tap" />
+      <BigBox ref={bigBoxRef}>
+        <Box
+          variants={boxVariants}
+          whileHover="hover"
+          whileTap="tap"
+          drag
+          dragSnapToOrigin
+          dragConstraints={bigBoxRef}
+        />
+      </BigBox>
     </Wrapper>
   );
 }
