7a3890a6050b6bdb9e7a510134780ceed6ec223a
Choco Kim <devheedoo@gmail.com>
2021-12-18 01:39:13 +0900

    feat(7.9): Scale with useViewportScroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import { motion, useMotionValue, useTransform, Variants } from 'framer-motion';
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  useViewportScroll,
+  Variants,
+} from 'framer-motion';
 import styled from 'styled-components';
 
 const Wrapper = styled(motion.div)`
   width: 100vw;
-  height: 100vh;
+  height: 200vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -25,7 +31,6 @@ const boxVariants: Variants = {
 
 function App() {
   const x = useMotionValue(0);
-  const scale = useTransform(x, [-400, 0, 400], [0.1, 1, 2]);
   const gradient = useTransform(
     x,
     [-400, 0, 400],
@@ -39,6 +44,8 @@ function App() {
   // useEffect(() => {
   //   x.onChange(() => console.log(x.get()));
   // }, [x]);
+  const { scrollYProgress } = useViewportScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 3]);
 
   return (
     <Wrapper style={{ background: gradient }}>
