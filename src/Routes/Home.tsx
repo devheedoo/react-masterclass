2281011018aca16df1eba4d0efa12673c0e2d66e
Choco Kim <devheedoo@gmail.com>
2021-12-18 10:50:41 +0900

    feat(8.1): header #1

---
+++ src/Routes/Home.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(135deg, cyan, lightgreen);
+`;
+
+export default function Home() {
+  return <Wrapper></Wrapper>;
+}
