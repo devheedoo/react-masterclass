9a36e65e40d6a86a3ab5c4420870adbfd11b3edd
Choco Kim <devheedoo@gmail.com>
2021-12-19 17:11:57 +0900

    feat(8.9-8.10): Add image and title to slider
    - pagination using offset
    - animation on hover
    - dynamic slider offset by window.innerWidth

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -5,6 +5,9 @@ import styled from 'styled-components';
 import { fetchMoviesNowPlaying, IMoviesNowPlaying } from '../api';
 import { makeMovieImageUrl } from '../utils';
 
+const SLIDER_OFFSET = Math.floor(window.innerWidth / 300);
+
+/* styled components */
 const Wrapper = styled.div`
   width: 100vw;
   height: 200vh;
@@ -35,29 +38,68 @@ const Overview = styled.p`
 `;
 
 const Slider = styled.div`
-  position: relative;
+  /* position: relative; */
   background-color: red;
 `;
 
-const Row = styled(motion.div)`
+const Row = styled(motion.div)<{ offset: number }>`
   position: absolute;
   display: grid;
   width: 100%;
   gap: 10px;
-  grid-template-columns: repeat(6, 1fr);
+  grid-template-columns: repeat(${(props) => props.offset}, 1fr);
 `;
 
-const Box = styled(motion.div)`
-  background-color: rgba(255, 255, 255, 1);
-  color: black;
+const Box = styled(motion.div)<{ fileNameWithExtension: string }>`
+  aspect-ratio: 1.58;
+  position: relative;
   flex: 1;
-  height: 120px;
+  &:first-child {
+    transform-origin: center left;
+  }
+  &:last-child {
+    transform-origin: center right;
+  }
+  background-size: cover;
+  background-image: url(${(props) =>
+    makeMovieImageUrl(props.fileNameWithExtension, 300)});
 `;
 
+const BoxDescription = styled(motion.div)`
+  height: 50px;
+  background-color: ${(props) => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  h4 {
+    text-align: center;
+    color: ${(props) => props.theme.white.darker};
+  }
+`;
+
+/* variants */
 const rowVariants: Variants = {
   initial: { x: window.outerWidth + 10 },
   animate: { x: 0 },
   exit: { x: -window.outerWidth - 10 },
+};
+
+const boxVariants: Variants = {
+  initial: { scale: 1, y: 0 },
+  hover: {
+    scale: 1.2,
+    y: -80,
+    zIndex: 99,
+    transition: { delay: 0.3, duration: 0.3 },
+  },
+};
+
+const boxDescriptionVariants: Variants = {
+  hover: { opacity: 1, transition: { delay: 0.3, duration: 0.3 } },
 };
 
 export default function Home() {
@@ -67,16 +109,15 @@ export default function Home() {
   );
   const [movieOfBanner, ...moviesOfSlider] = data?.results ?? [];
 
-  const sampleSlide = [1, 2, 3, 4, 5, 6];
-
   const [isExitingSlide, setExitingSlide] = useState(false);
   const handleExitComplete = () => setExitingSlide(false);
 
   const [sliderIndex, setSliderIndex] = useState(0);
   const showNextSlide = () => {
-    if (isExitingSlide) return;
+    if (isExitingSlide) return; // Disable while slide animtion playing
+    const maxIndex = Math.ceil(moviesOfSlider.length / SLIDER_OFFSET) - 1;
     setExitingSlide(true);
-    setSliderIndex((prev) => (prev === sampleSlide.length - 1 ? 0 : prev + 1));
+    setSliderIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
   };
 
   return (
@@ -95,6 +136,7 @@ export default function Home() {
               onExitComplete={handleExitComplete}
             >
               <Row
+                offset={SLIDER_OFFSET}
                 variants={rowVariants}
                 initial="initial"
                 animate="animate"
@@ -102,9 +144,28 @@ export default function Home() {
                 exit="exit"
                 key={sliderIndex}
               >
-                {sampleSlide.map((e) => (
-                  <Box key={e}>{e}</Box>
-                ))}
+                {moviesOfSlider
+                  .slice(
+                    SLIDER_OFFSET * sliderIndex,
+                    SLIDER_OFFSET * (sliderIndex + 1)
+                  )
+                  .map((movie) => (
+                    <Box
+                      key={movie.id}
+                      fileNameWithExtension={movie.backdrop_path}
+                      variants={boxVariants}
+                      initial="initial"
+                      whileHover="hover"
+                      transition={{ type: 'tween' }}
+                    >
+                      <BoxDescription
+                        variants={boxDescriptionVariants}
+                        transition={{ type: 'tween' }}
+                      >
+                        <h4>{movie.title}</h4>
+                      </BoxDescription>
+                    </Box>
+                  ))}
               </Row>
             </AnimatePresence>
           </Slider>
