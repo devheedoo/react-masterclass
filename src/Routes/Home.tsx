8f8706aa2669e0969a2a569f971f6b248bc3ff8a
Choco Kim <devheedoo@gmail.com>
2021-12-20 01:35:52 +0900

    feat(8.12): movie modal #2
    - Add overlay and remove modal when overlay clicked
    - Fix top position of modal

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,4 +1,4 @@
-import { AnimatePresence, motion } from 'framer-motion';
+import { AnimatePresence, motion, useViewportScroll } from 'framer-motion';
 import { useState } from 'react';
 import { useQuery } from 'react-query';
 import { useHistory, useRouteMatch } from 'react-router-dom';
@@ -39,15 +39,24 @@ const Overview = styled.p`
   width: 70%;
 `;
 
+const Overlay = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  opacity: 0;
+`;
+
 const MovieDetailModal = styled(motion.div)`
   position: absolute;
-  top: 100px;
   left: 0;
   right: 0;
   margin: 0 auto;
   width: 40vw;
   height: 40vw;
-  background-color: rgba(255, 255, 0, 0.8);
+  background-color: rgba(165, 165, 115, 0.8);
+  z-index: 101;
 `;
 
 export default function Home() {
@@ -80,6 +89,9 @@ export default function Home() {
     history.push(`/movie/${movieId}`);
   };
 
+  const { scrollY } = useViewportScroll();
+  const handleClickOverlay = () => history.push('/');
+
   return (
     <Wrapper>
       {isLoading ? (
@@ -102,7 +114,17 @@ export default function Home() {
           />
           <AnimatePresence>
             {matchesMovieId ? (
-              <MovieDetailModal layoutId={matchesMovieId?.params.movieId} />
+              <>
+                <Overlay
+                  onClick={handleClickOverlay}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                />
+                <MovieDetailModal
+                  style={{ top: scrollY.get() + 100 }}
+                  layoutId={matchesMovieId?.params.movieId}
+                />
+              </>
             ) : null}
           </AnimatePresence>
         </>
